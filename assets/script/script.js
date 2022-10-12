0a467fe706105447986c4ec51b565a216fba7708
Lambo95 <lamberto.martini95@gmail.com>
2022-10-12 09:27:32 +0200

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,11 @@
 function changeTheme(event) {
+  let footer = document.getElementsByClassName("footer");
+
   if (event.target.checked) {
-    document.body.classList.add(".dark-theme");
+    document.body.classList.add("dark-theme");
+    footer.classList.add("dark-theme");
   } else {
-    document.body.classList.remove(".dark-theme");
+    document.body.classList.remove("dark-theme");
+    footer.classList.remove("dark-theme");
   }
 }
